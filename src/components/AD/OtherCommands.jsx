ac898a6834ef0f9049443082f34fa78c2753d727
lvkhanh <lvkhanh@lhv.vn>
2020-11-05 15:56:41 +0700

    Switch to embedded mode

--- src/components/AD/OtherCommands.jsx
+++ src/components/AD/OtherCommands.jsx
@@ -4,6 +4,7 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import { CARD_STYLE } from "../../styles";
+import { CardContent } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   ...CARD_STYLE(theme),
@@ -33,8 +34,30 @@ export default function OtherCommands({ sendCommand }) {
     sendCommand(command, event.currentTarget.id);
   };
 
+  const handleSwitchToEmbeddedAction = (event) => {
+    event.preventDefault();
+    let location = window.location.href;
+    if (!location.includes("embedded")) {
+      location = location.replace("analyze", "analyze/embedded");
+    }
+
+    window.location.replace(location);
+  };
+
   return (
     <Card className={classes.card} variant="outlined">
+      <CardContent className={classes.cardContent}>
+        <Button
+          size="medium"
+          className={classes.button}
+          id="switchToEmbedded"
+          variant="outlined"
+          color={"secondary"}
+          onClick={handleSwitchToEmbeddedAction}
+        >
+          Switch To Embedded Mode
+        </Button>
+      </CardContent>
       <CardActions className={classes.cardActions}>
         <Button
           size="small"
